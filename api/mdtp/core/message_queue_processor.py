1b0590bd97667f10477a87cc09a768da5086c132
Krishan Patel <krishan711@gmail.com>
2021-04-07 20:16:31 +0100

    .

--- api/mdtp/core/message_queue_processor.py
+++ api/mdtp/core/message_queue_processor.py
@@ -30,7 +30,6 @@ class MessageQueueProcessor:
                 time.sleep(30)
             else:
                 logging.info(f'MESSAGE - {message.command} {message.content}')
-                # await self.slackClient.post(text=f'MESSAGE - {message.command} {message.content}')
                 startTime = time.time()
                 statusCode = 200
                 try:
@@ -44,5 +43,3 @@ class MessageQueueProcessor:
                     # TODO(krish): should possibly reset the visibility timeout
                 duration = time.time() - startTime
                 logging.info(f'MESSAGE - {message.command} {message.content} - {statusCode} - {duration}')
-                if statusCode != 200:
-                    await self.slackClient.post(text=f'MESSAGE - {message.command} {message.content} - {statusCode} - {duration}')
