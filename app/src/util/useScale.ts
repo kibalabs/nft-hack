5c39c629c1ae678fe6969a78e640cb4609798f52
Krishan Patel <krishan711@gmail.com>
2021-04-25 18:44:51 +0100

    .

--- app/src/util/useScale.ts
+++ app/src/util/useScale.ts
@@ -2,45 +2,31 @@ import React from 'react';
 
 import { useEventListener } from '@kibalabs/core-react';
 
-type ScaleOpts = {
-  direction: 'up' | 'down';
-  interval: number;
-}
-
 export const useScale = (ref: React.RefObject<HTMLElement | null>, minScale: number, maxScale: number): [number, (newScale: number) => void] => {
   const [scale, setScale] = React.useState<number>(1);
 
-  const updateScale = ({ direction, interval }: ScaleOpts) => {
-    setScale((currentScale: number): number => {
-      let newScale = currentScale;
-      // if (direction === 'up' && currentScale + interval < maxScale) {
-      //   newScale = currentScale + interval;
-      // } else
-      if (direction === 'up') {
-        newScale = Math.min(currentScale + interval, maxScale);
-      } else
-      // if (direction === 'down' && currentScale - interval > minScale) {
-      //   newScale = currentScale - interval;
-      // } else
-      if (direction === 'down') {
-        newScale = Math.max(currentScale - interval, minScale);
-      }
-      return newScale;
-    });
+  const constrainScale = (newScale: number): number => {
+    return Math.min(Math.max(newScale, minScale), maxScale);
+  };
+
+  const setScaleManually = (newScale: number): void => {
+    setScale(constrainScale(newScale));
   };
 
   useEventListener(ref.current, 'wheel', (e) => {
     e.preventDefault();
-    updateScale({
+    setScale((currentScale: number): number => {
+      let newScale = currentScale;
       // @ts-ignore
-      direction: e.deltaY > 0 ? 'up' : 'down',
-      interval: 0.1,
+      if (e.deltaY > 0) {
+        newScale = currentScale + 0.1;
+      // @ts-ignore
+      } else if (e.deltaY < 0) {
+        newScale = currentScale - 0.1;
+      }
+      return constrainScale(newScale);
     });
   });
-
-  const setScaleManually = (newScale: number): void => {
-    setScale(Math.min(Math.max(newScale, minScale), maxScale));
-  };
 
   return [scale, setScaleManually];
 };
