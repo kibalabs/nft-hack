e68faf028d235d9a5397e62c4167deac2d660e58
A Fox <arthur.fox@hotmail.co.uk>
2021-04-29 19:34:03 -0300

    Feature - Created a share button, this is not yet functional though

--- app/src/components/RightHandSideButtons.tsx
+++ app/src/components/RightHandSideButtons.tsx
@@ -10,8 +10,14 @@ export const RightHandSideButtons = (): React.ReactElement => {
     navigator.navigateTo('/about');
   };
 
+  const onShareClicked = () => {
+    navigator.navigateTo('/about');
+  };
+
   return (
     <Box variant='overlay-topLeftCutoff'>
+      <Button variant={'primary'} text='' iconGutter={PaddingSize.None} iconRight={<KibaIcon iconId='ion-share' />} onClicked={onShareClicked} />
+      <Spacing variant={PaddingSize.Narrow} />
       <Button variant={'primary'} text='' iconGutter={PaddingSize.None} iconRight={<KibaIcon iconId='ion-cart' />} target={'https://testnets.opensea.io/collection/mdtp-test-2?embed=true'} />
       <Spacing variant={PaddingSize.Narrow} />
       <Button variant={'primary'} text='' iconGutter={PaddingSize.None} iconRight={<KibaIcon iconId='ion-help' />} onClicked={onAboutClicked} />
