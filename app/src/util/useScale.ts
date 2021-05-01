95942795981064ff85ed480e67aee2e9f5064519
Krishan Patel <krishan711@gmail.com>
2021-05-01 16:57:58 +0100

    .

--- app/src/util/useScale.ts
+++ app/src/util/useScale.ts
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { useEventListener } from '@kibalabs/core-react';
 
-export const useScale = (ref: React.RefObject<HTMLElement | null>, minScale: number, maxScale: number, scaleRate: number = 0.1, shouldInvert: boolean = false): [number, (newScale: number) => void] => {
+export const useScale = (ref: React.RefObject<HTMLElement | null>, minScale: number, maxScale: number, scaleRate = 0.1, shouldInvert = false): [number, (newScale: number) => void] => {
   const [scale, setScale] = React.useState<number>(1);
   const increment = shouldInvert ? -scaleRate : scaleRate;
 
