e2b464cfed8ce11a63e83701d6a7883823b21ba5
bgok <ken@citizenhodler.com>
2021-06-24 18:12:00 -0700

    WIP Staking Wallet compile, still a couple of TODOs.

--- blockchain/contracts/StakingWallet.sol
+++ blockchain/contracts/StakingWallet.sol
@@ -1,56 +1,89 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.3;
 
-import "@openzeppelin/contracts/payment/escrow/Escrow.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract StakingWallet is Escrow, Ownable {
+abstract contract ICERC20 {
+    function redeemUnderlying(uint redeemAmount) external virtual returns (uint);
+    function mint(uint mintAmount) external virtual returns (uint);
+    function exchangeRateCurrent() public view virtual returns(uint); // TODO This is part of CToken. How do they relate to each other?
+}
 
-    CErc20 cToken = CToken(0x3FDA...);
-    unit256 totalOnDeposit = 0;
+// This is meant to be constructed and owned by an ERC721 contract
+contract StakingWallet is Ownable {
+    using SafeMath for uint256;
+    using Address for address payable;
 
-    override event Withdrawn(address indexed payee, unint256 tokenId, uint256 weiAmount);
+    IERC721 collection;
+    ICERC20 cToken;
+    IERC20 underlyingERC20;
 
-    function validTokenId() {}
+    event Deposited(uint256 tokenId, address indexed payer, uint256 weiAmount);
+    event Withdrawn(uint256 tokenId, address indexed payee, uint256 weiAmount);
+    event InterestClaimed(address indexed payee, uint256 weiAmount);
 
-    constructor() {
-        cToken.approve(...);
-        super();
+    modifier validTokenId(uint256 tokenId) {
+        collection.ownerOf(tokenId); // Throws if the token doesn't exist
+        _;
     }
 
-    public override deposit(uint256 tokenId) public payable virtual onlyOwner validTokenId{
-        super.deposit(address(tokenId))
-        totalOnDeposit += msg.amount;
-        cToken.mint(msg.amount);
+    constructor(address _cTokenAddress, address _underlying) Ownable() {
+        collection = IERC721(msg.sender);
+        underlyingERC20 = IERC20(_underlying);
+        cToken = ICERC20(_cTokenAddress);
     }
 
-    public withdrawStake(address payable payee, uint256 tokenId) public virtual onlyOwner {
-        uint256 deposits = _deposits[tokenId];
+    mapping(uint256 => uint256) private _deposits;
+    uint256 totalOnDeposit = 0;
+
+    function depositsOf(uint256 tokenId) public view returns (uint256) {
+        return _deposits[tokenId];
+    }
+
+    function deposit(uint256 tokenId) public payable virtual onlyOwner validTokenId(tokenId) {
+        // TODO This should be sending and recieving an ERC20, not ether
+        uint256 amount = msg.value;
+        _deposits[tokenId] += amount;
+        totalOnDeposit += amount;
+        _mint(amount);
+
+        emit Deposited(tokenId, tx.origin, amount);
+    }
+
+    function withdraw(address payable payee, uint256 tokenId) public onlyOwner validTokenId(tokenId) {
+        // TODO This should be sending and recieving an ERC20, not ether
+        uint256 onDeposit = _deposits[tokenId];
         _deposits[tokenId] = 0;
-        totalOnDeposit -= deposits;
+        totalOnDeposit -= onDeposit;
 
-        cToken.redeemUnderlying(deposits);
+        cToken.redeemUnderlying(onDeposit);
 
-        payee.sendValue(deposits);
-        emit Withdrawn(payee, tokenId, deposits);
+        payee.sendValue(onDeposit);
+
+        emit Withdrawn(tokenId, payee, onDeposit);
     }
 
-    public override withdraw() {
-        throw()
-    }
-
-    public sendAccumulatedInterest(address payable payee, uint256 claimToken) onlyOwner {
-        // TODO validate the claim token
-
+    function sendAccumulatedInterest(address payable payee) public onlyOwner {
+        // TODO This should be sending and recieving an ERC20, not ether
         uint256 earnedInterest = getInterestEarned();
         cToken.redeemUnderlying(earnedInterest);
 
         payee.sendValue(earnedInterest);
-        emit RewardWithdrawn(payee, earnedInterest);
+        emit InterestClaimed(payee, earnedInterest);
     }
 
-    public getInterestEarned() returns (uint256) {
+    function getInterestEarned() public view returns (uint256) {
         uint exchangeRateMantissa = cToken.exchangeRateCurrent();
-        uint256 reserved = totalOnDeposit / exchangeRateMantissa;
-        return reserved;
+        uint256 interest = totalOnDeposit / exchangeRateMantissa;
+        return interest;
+    }
+
+    function _mint(uint amount) private {
+        underlyingERC20.approve(address(cToken), amount);
+        assert(cToken.mint(100) == 0);            // mint the cTokens and assert there is no error
     }
 }
