8c510efcfd578f3153f19dde00b993300c580ec1
A Fox <arthur.fox@hotmail.co.uk>
2021-07-08 18:45:48 -0300

    Text is being drawn now

--- api/create_metadata.py
+++ api/create_metadata.py
@@ -22,6 +22,10 @@ def genImage(tokenId: int, outputDirectory: str):
     if not os.path.exists(outputDirectory):
         os.makedirs(outputDirectory)
 
+    # Colour
+    grey = 255
+    white = (255,255,255,255)
+
     # New Image
     newImage = Image.new('RGBA', (IMAGE_HEIGHT_AND_WIDTH, IMAGE_HEIGHT_AND_WIDTH), "black")  
     newImageDraw = ImageDraw.Draw(newImage)
@@ -29,16 +33,21 @@ def genImage(tokenId: int, outputDirectory: str):
     # Draw rows and columns
     totalRowCol = int(IMAGE_HEIGHT_AND_WIDTH/10)
     for row in range(0, totalRowCol):
-      newImageDraw.line((0, row*10, IMAGE_HEIGHT_AND_WIDTH, row*10), fill=255)
+      newImageDraw.line((0, row*10, IMAGE_HEIGHT_AND_WIDTH, row*10), fill=grey)
 
     for col in range(0, totalRowCol):
-      newImageDraw.line((col*10, 0, col*10, IMAGE_HEIGHT_AND_WIDTH), fill=255)
+      newImageDraw.line((col*10, 0, col*10, IMAGE_HEIGHT_AND_WIDTH), fill=grey)
 
     # Draw Box at Token Number
-    xCoord = tokenId % 100
-    yCoord = int(tokenId / 100)
+    xBox = tokenId % 100
+    yBox = int(tokenId / 100)
     # logging.info(f'x: {xCoord} ; y: {yCoord}')
-    newImageDraw.rectangle((xCoord*10, yCoord*10, xCoord*10 + 10, yCoord*10 + 10), fill=255)
+    newImageDraw.rectangle((xBox*10, yBox*10, xBox*10 + 10, yBox*10 + 10), fill=white)
+
+    # Draw Text for MDTP and Token Number
+    font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 80)
+    newImageDraw.multiline_text((80,80), "Million\nDollar\nToken\nPage", font=font, fill=white)
+    newImageDraw.multiline_text((650,800), f'Token\n{tokenId}', font=font, fill=white)
     
     newImage.save(os.path.join(outputDirectory, f'metadata-{tokenId}.png'))
   
