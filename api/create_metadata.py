a4f870339b5461f5dbbe866e731525472b551947
A Fox <arthur.fox@hotmail.co.uk>
2021-07-08 18:16:08 -0300

    Script generates all rows and columns

--- api/create_metadata.py
+++ api/create_metadata.py
@@ -1,5 +1,5 @@
 import asyncio
-# import os
+import os
 # import json
 import logging
 # import math
@@ -14,12 +14,34 @@ import asyncclick as click
 # from core.requester import Requester
 # from core.s3_manager import S3Manager
 # from core.web3.eth_client import RestEthClient
-# from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
+IMAGE_HEIGHT_AND_WIDTH = 1000
+
+def genImage(tokenId: int, outputDirectory: str):
+    if not os.path.exists(outputDirectory):
+        os.makedirs(outputDirectory)
+
+    newImage = Image.new('RGBA', (IMAGE_HEIGHT_AND_WIDTH, IMAGE_HEIGHT_AND_WIDTH), "black")  
+    newImageDraw = ImageDraw.Draw(newImage)
+        
+    totalRowCol = int(IMAGE_HEIGHT_AND_WIDTH/10)
+    for row in range(0, totalRowCol):
+      newImageDraw.line((0, row*10, IMAGE_HEIGHT_AND_WIDTH, row*10), fill=255)
+
+    for col in range(0, totalRowCol):
+      newImageDraw.line((col*10, 0, col*10, IMAGE_HEIGHT_AND_WIDTH), fill=255)
+    
+    newImage.save(os.path.join(outputDirectory, f'metadata-{tokenId}.png'))
+  
 
 @click.command()
 @click.option('-t', '--token-id', 'tokenId', required=True, type=int)
 async def run(tokenId: int):
-    logging.info(f'TokenID: {tokenId}')    
+    logging.info(f'TokenID: {tokenId}')
+    outputDirectory = 'output'
+    genImage(tokenId=tokenId, outputDirectory=outputDirectory)
+    logging.info(f'Complete!')
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
