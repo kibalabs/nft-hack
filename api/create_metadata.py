6d6cd85a66a8b694e64cb3a556201d1e750f05c1
A Fox <arthur.fox@hotmail.co.uk>
2021-07-08 19:11:37 -0300

    Text flips based on the quadrant our token is in

--- api/create_metadata.py
+++ api/create_metadata.py
@@ -22,9 +22,10 @@ def genImage(tokenId: int, outputDirectory: str):
     if not os.path.exists(outputDirectory):
         os.makedirs(outputDirectory)
 
-    # Colour
+    # Colours and Fonts
     grey = 255
     white = (255,255,255,255)
+    font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 80)
 
     # New Image
     newImage = Image.new('RGBA', (IMAGE_HEIGHT_AND_WIDTH, IMAGE_HEIGHT_AND_WIDTH), "black")  
@@ -39,15 +40,22 @@ def genImage(tokenId: int, outputDirectory: str):
       newImageDraw.line((col*10, 0, col*10, IMAGE_HEIGHT_AND_WIDTH), fill=grey)
 
     # Draw Box at Token Number
-    xBox = tokenId % 100
-    yBox = int(tokenId / 100)
-    # logging.info(f'x: {xCoord} ; y: {yCoord}')
-    newImageDraw.rectangle((xBox*10, yBox*10, xBox*10 + 10, yBox*10 + 10), fill=white)
+    xCoord = (tokenId-1) % 100
+    yCoord = int( (tokenId-1) / 100)
+    logging.info(f'x: {xCoord} ; y: {yCoord}')
+    newImageDraw.rectangle((xCoord*10, yCoord*10, xCoord*10 + 10, yCoord*10 + 10), fill=white)
 
-    # Draw Text for MDTP and Token Number
-    font = ImageFont.truetype("/Library/Fonts/Arial.ttf", 80)
-    newImageDraw.multiline_text((80,80), "Million\nDollar\nToken\nPage", font=font, fill=white)
-    newImageDraw.multiline_text((650,800), f'Token\n{tokenId}', font=font, fill=white)
+    # Draw Text for MDTP and Token Number        
+    inTopLeftQuadrant = xCoord < 50 and yCoord < 50
+    inBottomRightQuadrant = xCoord > 50 and yCoord > 50
+    flipText = inTopLeftQuadrant or inBottomRightQuadrant
+
+    if flipText:
+      newImageDraw.multiline_text((650,80), "Million\nDollar\nToken\nPage", font=font, fill=white)
+      newImageDraw.multiline_text((80,800), f'Token\n{tokenId}', font=font, fill=white)
+    else:
+      newImageDraw.multiline_text((80,80), "Million\nDollar\nToken\nPage", font=font, fill=white)
+      newImageDraw.multiline_text((650,800), f'Token\n{tokenId}', font=font, fill=white)
     
     newImage.save(os.path.join(outputDirectory, f'metadata-{tokenId}.png'))
   
