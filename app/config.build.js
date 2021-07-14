01128d6fa04abfba2b92c05eb7c9a8752b042555
Krishan Patel <krishan711@gmail.com>
2021-07-14 23:02:52 +0100

    .

--- app/config.build.js
+++ app/config.build.js
@@ -1,82 +1,22 @@
 /* eslint-disable */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CreateRuntimeConfigPlugin = require('@kibalabs/build/scripts/plugins/createRuntimeConfigPlugin')
+const InjectSeoPlugin = require('@kibalabs/build/scripts/plugins/injectSeoPlugin');
 
 const title = 'Million Dollar Token Page';
 const description = 'MillionDollarTokenPage (MDTP) is a digital advertising space powered by the Ethereum cryptocurrency network and NFT technology. Each pixel block you see can be purchased as a unique NFT, set to display what you like, and later re-sold on the secondary-market. So join us and interact, trade and share, and make crypto history!';
 const imageUrl = 'https://wml-images.s3-eu-west-1.amazonaws.com/mdtp-banner.png';
+const url = 'https://milliondollartokenpage.com'
 
-class HtmlWebpackInjectSeo {
-  constructor(options) {
-    this.options = options;
-  }
-  apply(compiler) {
-    compiler.hooks.compilation.tap('HtmlWebpackInjectSeo', compilation => {
-      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('HtmlWebpackInjectSeo.beforeEmit', (htmlPluginData, callback) => {
-        htmlPluginData.html = htmlPluginData.html.replace(/<title>.*<\/title>/i, `<title>${title}</title>`);
-        callback(null, htmlPluginData);
-      });
-      HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync('HtmlWebpackInjectSeo.alterAssetTagGroups', (htmlPluginData, callback) => {
-        htmlPluginData.headTags.push({
-          tagName: 'meta',
-          attributes: {
-            "name": "og:title",
-            "content": title,
-          },
-        });
-        htmlPluginData.headTags.push({
-          tagName: 'meta',
-          attributes: {
-            "name": "description",
-            "content": description,
-          },
-        });
-        htmlPluginData.headTags.push({
-          tagName: 'meta',
-          attributes: {
-            "name": "og:description",
-            "content": description,
-          },
-        });
-        htmlPluginData.headTags.push({
-          tagName: 'meta',
-          attributes: {
-            "name": "og:url",
-            "content": "https://milliondollartokenpage.com",
-          },
-        });
-        htmlPluginData.headTags.push({
-          tagName: 'meta',
-          attributes: {
-            "name": "og:image",
-            "content": imageUrl,
-          },
-        });
-        htmlPluginData.headTags.push({
-          tagName: 'meta',
-          attributes: {
-            "name": "twitter:card",
-            "content": "summary_large_image",
-          },
-        });
-        callback(null, htmlPluginData);
-      });
-    });
-  }
-}
+const seoTags = [
+  new InjectSeoPlugin.MetaTag('og:title', title),
+  new InjectSeoPlugin.MetaTag('description', description),
+  new InjectSeoPlugin.MetaTag('og:description', description),
+  new InjectSeoPlugin.MetaTag('twitter:card', 'summary_large_image'),
+  new InjectSeoPlugin.MetaTag('og:image', imageUrl),
+  new InjectSeoPlugin.MetaTag('og:url', url),
+];
 
 module.exports = (config) => {
-  config.webpackConfigModifier = (webpackConfig) => {
-    webpackConfig.resolve = webpackConfig.resolve || {};
-    webpackConfig.plugins = [
-      ...webpackConfig.plugins.filter((plugin) => !(plugin instanceof CreateRuntimeConfigPlugin)),
-      new CreateRuntimeConfigPlugin({
-        'KRT_CONTRACT_ADDRESS': process.env.KRT_CONTRACT_ADDRESS,
-        'KRT_API_URL': process.env.KRT_API_URL,
-      }),
-      new HtmlWebpackInjectSeo(),
-    ];
-    return webpackConfig;
-  }
+  config.seoTags = seoTags;
+  config.title = title;
   return config;
 };
