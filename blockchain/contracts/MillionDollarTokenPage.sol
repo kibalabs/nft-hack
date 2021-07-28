9c0a2a85e948eb63b454c3e0907b73917695cc78
Krishan Patel <krishan711@gmail.com>
2021-07-28 20:53:35 +0100

    .

--- blockchain/contracts/MillionDollarTokenPage.sol
+++ blockchain/contracts/MillionDollarTokenPage.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
-// import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 contract AdminManageable {
@@ -22,21 +23,34 @@ contract AdminManageable {
 }
 
 contract MillionDollarTokenPage is ERC721, IERC721Enumerable, AdminManageable {
+    using SafeMath for uint256;
+    using Strings for uint256;
 
     mapping (uint256 => string) private _tokenContentURIs;
-    mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
-    mapping(uint256 => uint256) private _ownedTokensIndex;
+    mapping (address => mapping(uint256 => uint256)) private _ownedTokens;
+    mapping (uint256 => uint256) private _ownedTokensIndex;
+    uint256[] private _mintedTokenIds;
 
     event TokenContentURIChanged(uint256 indexed tokenId);
+
+    uint16 public constant SUPPLY_LIMIT = 10000;
+    uint16 public totalMintLimit = 200;
+    uint16 public singleMintLimit = 20;
+    // uint256 public constant mintPrice = 50000000000000000; // 0.05 ETH
+    uint256 public constant mintPrice = 0; // 0.05 ETH
+
+    constructor() ERC721("MillionDollarTokenPage", "\u22A1") {
+        _admins[_msgSender()] = true;
+    }
+
+    // Ownership
 
     modifier onlyTokenOwner(uint256 tokenId) {
         require(ownerOf(tokenId) == _msgSender(), "MDTP: caller is not the tokenOwner");
         _;
     }
 
-    constructor() ERC721("MillionDollarTokenPage", "\u22A1") {
-        _admins[_msgSender()] = true;
-    }
+    // Metadata URIs
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://api.mdtp.co/token-metadatas/";
@@ -46,6 +60,8 @@ contract MillionDollarTokenPage is ERC721, IERC721Enumerable, AdminManageable {
         string memory baseURI = _baseURI();
         return string(abi.encodePacked(baseURI, Strings.toString(tokenId)));
     }
+
+    // Content URIs
 
     function _defaultBaseContentURI() internal pure returns (string memory) {
         return "https://api.mdtp.co/token-default-contents/";
@@ -64,10 +80,34 @@ contract MillionDollarTokenPage is ERC721, IERC721Enumerable, AdminManageable {
         return string(abi.encodePacked(_defaultBaseContentURI(), Strings.toString(tokenId)));
     }
 
-    function mint(address recipient, uint256 tokenId) public onlyAdmin returns (uint256) {
+    // Minting
+
+    function mintAdmin(uint256 tokenId) public onlyAdmin {
+        _mint(tokenId);
+    }
+
+    function mint(uint256 tokenId) public payable {
+        require(msg.value >= mintPrice, "MDTP: Insufficient payment");
+        require(mintedCount() + 1 <= totalMintLimit, "MDTP: reached current minting limit");
+        _mint(tokenId);
+    }
+
+    function mintMany(uint256[] memory tokenIds) public payable {
+        require(msg.value >= mintPrice.mul(tokenIds.length), "MDTP: Insufficient payment");
+        require(mintedCount() + tokenIds.length <= totalMintLimit, "MDTP: reached current minting limit");
+        require(tokenIds.length <= singleMintLimit, "MDTP: tokenIds.length is over singleMintLimit");
+        for(uint i = 0; i < tokenIds.length; i++) {
+            _mint(tokenIds[i]);
+        }
+    }
+
+    function _mint(uint256 tokenId) internal {
         require(tokenId > 0 && tokenId <= 10000, "MDTP: invalid tokenId");
-        super._safeMint(recipient, tokenId);
-        return tokenId;
+        super._safeMint(msg.sender, tokenId);
+    }
+
+    function mintedCount() public view returns (uint256) {
+        return _mintedTokenIds.length;
     }
 
     // IERC721Enumerable
@@ -87,7 +127,9 @@ contract MillionDollarTokenPage is ERC721, IERC721Enumerable, AdminManageable {
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override {
         super._beforeTokenTransfer(from, to, tokenId);
-        if (from != address(0)) {
+        if (from == address(0)) {
+            _mintedTokenIds.push(tokenId);
+        } else {
             uint256 lastTokenIndex = ERC721.balanceOf(from) - 1;
             uint256 tokenIndex = _ownedTokensIndex[tokenId];
             // If any token except the last is being removed, swap it with the last one
@@ -99,7 +141,10 @@ contract MillionDollarTokenPage is ERC721, IERC721Enumerable, AdminManageable {
             delete _ownedTokensIndex[tokenId];
             delete _ownedTokens[from][lastTokenIndex];
         }
-        if (to != address(0)) {
+        if (to == address(0)) {
+            // If the token is burnt remove the metadata
+            delete _tokenContentURIs[tokenId];
+        } else if (to != from) {
             uint256 length = ERC721.balanceOf(to);
             _ownedTokens[to][length] = tokenId;
             _ownedTokensIndex[tokenId] = length;
--- blockchain/test/tests.js
+++ blockchain/test/tests.js
@@ -18,24 +18,24 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("should have a totalSupply of 10000 after tokens have been minted", async function() {
-    await mdtp.mint(myWallet.address, 1);
-    await mdtp.mint(myWallet.address, 2);
+    await mdtp.mint(1);
+    await mdtp.mint(2);
     const totalSupply = await mdtp.totalSupply();
     expect(totalSupply).to.equal(10000);
   });
 
   it("doesn't allow minting a token with id over 10000", async function() {
-    const transaction = mdtp.mint(myWallet.address, 10001);
+    const transaction = mdtp.mint(10001);
     await expect(transaction).to.be.reverted;
   });
 
   it("doesn't allow minting a token with id 0", async function() {
-    const transaction = mdtp.mint(myWallet.address, 0);
+    const transaction = mdtp.mint(0);
     await expect(transaction).to.be.reverted;
   });
 
   it("doesn't allow minting a token with negative id", async function() {
-    const transaction = mdtp.mint(myWallet.address, -100);
+    const transaction = mdtp.mint(-100);
     await expect(transaction).to.be.reverted;
   });
 
@@ -45,13 +45,13 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("should have the correct content uri for a minted token without an overwritten uri", async function() {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     const contentUri = await mdtp.tokenContentURI(100);
     expect(contentUri).to.equal("https://api.mdtp.co/token-default-contents/100");
   });
 
   it("allows the owner to change the content uri", async function() {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     const newUri = "https://google.com/tokens/100"
     await mdtp.setTokenContentURI(100, newUri);
   });
@@ -63,7 +63,7 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("should have the correct content uri for a minted token with an overwritten uri", async function() {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     const newUri = "https://google.com/tokens/100"
     await mdtp.setTokenContentURI(100, newUri);
     const contentUri = await mdtp.tokenContentURI(100);
@@ -71,7 +71,8 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("does not allow a non-owner to change the content uri", async function() {
-    await mdtp.mint(otherWallet.address, 100);
+    await mdtp.mint(100);
+    await mdtp.transferFrom(myWallet.address, otherWallet.address, 100)
     const newUri = "https://google.com/tokens/100";
     const transaction = mdtp.setTokenContentURI(100, newUri);
     await expect(transaction).to.be.reverted;
@@ -83,36 +84,30 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("should have the correct metadata uri for a minted token", async function() {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     const metadataUri = await mdtp.tokenURI(100);
     expect(metadataUri).to.equal("https://api.mdtp.co/token-metadatas/100");
   });
 
-  it("does not allow the owner to call mint", async function() {
-    await mdtp.mint(myWallet.address, 100);
-    const transaction = mdtp.mint(myWallet.address, 100);
-    await expect(transaction).to.be.reverted;
-  });
-
-  it("does not allow a non-owner to call mint", async function() {
-    await mdtp.mint(otherWallet.address, 100);
-    const transaction = mdtp.mint(myWallet.address, 100);
+  it("does not allow a token ot be minted twice", async function() {
+    await mdtp.mint(100);
+    const transaction = mdtp.mint(100);
     await expect(transaction).to.be.reverted;
   });
 
   it("emits a Transfer event when a token is minted", async function() {
-    const transaction = mdtp.mint(myWallet.address, 100);
+    const transaction = mdtp.mint(100);
     await expect(transaction).to.emit(mdtp, 'Transfer').withArgs('0x0000000000000000000000000000000000000000', myWallet.address, 100);
   });
 
   it("emits a Transfer event when a token is transferred", async function() {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     const transaction = mdtp.transferFrom(myWallet.address, otherWallet.address, 100);
     await expect(transaction).to.emit(mdtp, 'Transfer').withArgs(myWallet.address, otherWallet.address, 100);
   });
 
   it("emits a TokenContentURIChanged event when a token content is changed", async function() {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     const newUri = "https://google.com/tokens/100"
     const transaction = mdtp.setTokenContentURI(100, newUri);
     await expect(transaction).to.emit(mdtp, 'TokenContentURIChanged').withArgs(100);
@@ -124,15 +119,15 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("returns the correct value for the balance of a token holder", async function () {
-    await mdtp.mint(myWallet.address, 100);
-    await mdtp.mint(myWallet.address, 101);
-    await mdtp.mint(myWallet.address, 102);
+    await mdtp.mint(100);
+    await mdtp.mint(101);
+    await mdtp.mint(102);
     const balance = await mdtp.balanceOf(myWallet.address);
     expect(balance).to.equal(3);
   });
 
   it("returns the correct value for the balance of a transferred token", async function () {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     await mdtp.transferFrom(myWallet.address, otherWallet.address, 100);
     const balance1 = await mdtp.balanceOf(myWallet.address);
     expect(balance1).to.equal(0);
@@ -146,13 +141,13 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("returns the correct value for ownerOf of a minted token", async function () {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     const owner = await mdtp.ownerOf(100);
     expect(owner).to.equal(myWallet.address);
   });
 
   it("returns the correct value for ownerOf of a transferred token", async function () {
-    await mdtp.mint(myWallet.address, 100);
+    await mdtp.mint(100);
     await mdtp.transferFrom(myWallet.address, otherWallet.address, 100);
     const owner = await mdtp.ownerOf(100);
     expect(owner).to.equal(otherWallet.address);
@@ -169,17 +164,17 @@ describe("MillionDollarTokenPage contract", async function() {
   });
 
   it("returns the correct value for tokenOfOwnerByIndex of a token owner", async function () {
-    await mdtp.mint(myWallet.address, 100);
-    await mdtp.mint(myWallet.address, 101);
-    await mdtp.mint(myWallet.address, 102);
+    await mdtp.mint(100);
+    await mdtp.mint(101);
+    await mdtp.mint(102);
     const tokenId = await mdtp.tokenOfOwnerByIndex(myWallet.address, 1);
     expect(tokenId).to.equal(101);
   });
 
   it("returns the correct value for tokenOfOwnerByIndex after tokens are transferred", async function () {
-    await mdtp.mint(myWallet.address, 100);
-    await mdtp.mint(myWallet.address, 101);
-    await mdtp.mint(myWallet.address, 102);
+    await mdtp.mint(100);
+    await mdtp.mint(101);
+    await mdtp.mint(102);
     const tokenId = await mdtp.tokenOfOwnerByIndex(myWallet.address, 0);
     expect(tokenId).to.equal(100);
     await mdtp.transferFrom(myWallet.address, otherWallet.address, 100);
