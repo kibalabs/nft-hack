1a61c8bf76ab4fae13ca395357a47e4f2f4340e6
A Fox <arthur.fox@hotmail.co.uk>
2021-08-13 16:39:23 -0300

    Updated Button names

--- app/src/pages/HomePage/HomePage.tsx
+++ app/src/pages/HomePage/HomePage.tsx
@@ -161,11 +161,11 @@ export const HomePage = (): React.ReactElement => {
             {isMenuOpen && (
               <React.Fragment>
                 <Button variant='overlay' text='About MDTP' iconLeft={<KibaIcon iconId='ion-help-circle' />} onClicked={onAboutClicked} />
-                <Button variant='overlay' text='Roadmap' iconLeft={<KibaIcon iconId='ion-map' />} onClicked={onRoadmapClicked} />
+                <Button variant='overlay' text='View Roadmap' iconLeft={<KibaIcon iconId='ion-map' />} onClicked={onRoadmapClicked} />
                 <Button variant='overlay' text='Share MDTP' iconLeft={<KibaIcon iconId='ion-share' />} onClicked={onShareOpenClicked} />
                 <Button variant='overlay' text='Join Discord' iconLeft={<KibaIcon iconId='ion-logo-discord' />} target={'https://discord.gg/bUeQjW4KSN'} />
                 <Button variant='overlay' text='Follow Twitter' iconLeft={<KibaIcon iconId='ion-logo-twitter' />} target={'https://twitter.com/mdtp_app'} />
-                <Button variant='overlay' text='Marketplace' iconLeft={<KibaIcon iconId='ion-cart' />} target={getProductOpenseaUrl(network) || ''} />
+                <Button variant='overlay' text='Open Marketplace' iconLeft={<KibaIcon iconId='ion-cart' />} target={getProductOpenseaUrl(network) || ''} />
               </React.Fragment>
             )}
           </Stack>
