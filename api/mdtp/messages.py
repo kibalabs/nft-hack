f330f316a01316899774cc6fac7266eb1e22248b
Krishan Patel <krishan711@gmail.com>
2021-08-18 23:56:56 +0100

    .

--- api/mdtp/messages.py
+++ api/mdtp/messages.py
@@ -2,23 +2,23 @@ from core.queues.model import MessageContent
 
 
 class UpdateTokenMessageContent(MessageContent):
-    COMMAND = 'UPDATE_TOKEN'
+    _COMMAND = 'UPDATE_TOKEN'
     network: str
     tokenId: int
 
 class UpdateTokensMessageContent(MessageContent):
-    COMMAND = 'UPDATE_TOKENS'
+    _COMMAND = 'UPDATE_TOKENS'
     network: str
 
 class UpdateAllTokensMessageContent(MessageContent):
-    COMMAND = 'UPDATE_ALL_TOKENS'
+    _COMMAND = 'UPDATE_ALL_TOKENS'
     network: str
 
 class UploadTokenImageMessageContent(MessageContent):
-    COMMAND = 'UPLOAD_TOKEN_IMAGE'
+    _COMMAND = 'UPLOAD_TOKEN_IMAGE'
     network: str
     tokenId: int
 
 class BuildBaseImageMessageContent(MessageContent):
-    COMMAND = 'BUILD_BASE_IMAGE'
+    _COMMAND = 'BUILD_BASE_IMAGE'
     network: str
