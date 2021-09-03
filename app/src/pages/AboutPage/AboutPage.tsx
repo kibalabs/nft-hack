3a942a5a2de30f918aa9a47edb53139a5e40c669
A Fox <arthur.fox@hotmail.co.uk>
2021-09-03 13:28:46 +0100

    WIP improving text - split into Main and LearnMore

--- app/src/pages/AboutPage/AboutPage.tsx
+++ app/src/pages/AboutPage/AboutPage.tsx
@@ -4,10 +4,39 @@ import { Alignment, Direction, Markdown, PaddingSize, Stack, Text } from '@kibal
 
 
 export const AboutPage = (): React.ReactElement => {
-  const text = `
-  **What is Million Dollar Token Page? 🤔**
+  const mainText = `
 
-[Milliondollartokenpage.com](http://milliondollartokenpage.com) (MDTP) is [milliondollarhomepage.com](http://milliondollarhomepage.com) in the crypto era.
+**[Milliondollartokenpage.com](http://milliondollartokenpage.com) (MDTP) is [milliondollarhomepage.com](http://milliondollarhomepage.com) in the crypto era.**
+
+Flex and find the latest in NFTs and crypto!
+
+&nbsp;
+
+**Easy to use! ⚙️**
+
+Check out what's hot!
+
+![Blocks](/assets/pixel-block.mp4)
+
+Mint new blocks.
+
+![Mint](/assets/minting-block.mp4)
+
+Update blocks!
+
+![Update](/assets/update-block.mp4)
+
+
+Come join us now and own a piece of crypto history on milliondollartokenpage.com! ❤️
+`;
+
+const learnMoreText = `
+
+**Want to learn more? 💡**
+
+&nbsp;
+
+**What's changed? ⌛**
 
 When created in 2005 milliondollarhomepage acted as an advertising board on the internet, selling one million pixels on the web for $1 each. Back then the web was still very static and there were no digital native forms of transacting value so when pixels were bought and set, they could no longer be re-sold or updated.
 
@@ -15,25 +44,19 @@ Today the internet is a lot more fun to play with and cryptocurrencies like [Bit
 
 &nbsp;
 
-**How does it work? ⚙️**
+**How does MDTP work? ⛩️**
 
 MDTP has 10,000 blocks on a 1000 x 1000 2D grid represented by 10,000 NFTs on the Ethereum network. Each NFT can either be minted or bought second-hand from someone who's already minted it. Once owned, you can put any image within that block, along with a title, description and url to another website!
 
-![Blocks](/assets/pixel-block.mp4)
-
 To mint new blocks and make use of Ethereum and Web3 technologies you need to have a Web3 enabled plugin like [Metamask](https://metamask.io/) or browser like [Brave](https://brave.com/), which has a native crypto wallet, and to have some cryptocurrency in your wallet. You'll need [Ether](https://ethereum.org/en/eth/) in your wallet to pay for the [minting fees](https://postergrind.com/how-much-does-it-cost-to-mint-an-nft/) of the NFT collection and [mining fees](https://www.coindesk.com/learn/ethereum-101/ethereum-mining-works) of the network. Simply select "mint" in the side-panel for a block that's not yet been minted and confirm the transaction.
 
-![Mint](/assets/minting-block.mp4)
-
 Once you own the NFT that represents a block you can then change the image, title, description and url by clicking it and selecting "update token" on the side-panel. By connecting your wallet the site will recognises that you own that NFT and allow you to update the content associated with that block.
-
-![Update](/assets/update-block.mp4)
 
 And its a simple as that, buy blocks as NFTs and share your website, work or creations in this digital content space with the entire world!
 
 &nbsp;
 
-**Why should I get involved? 💡**
+**Why should I get involved? ⚖️**
 
 MDTP's magic comes in using the Ethereum network to store data and token ownership, as you get the benefit of both (1) life beyond the developers, and (2) the ability to re-sell your blocks easily. Let's expand on these benefits...
 
@@ -43,13 +66,13 @@ Secondly, as an owner of this NFT, you can use external marketplace like [opense
 
 The more people know about the site the more demand will rise and subsequently the price of your NFTs! So make sure to share the site with your friends and followers using our handy share button in the menu!
 
-Come along and join us now by interacting, trading and sharing on milliondollartokenpage.com. Let's make crypto history! ❤️
   `;
 
   return (
     <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start} isScrollableVertically={true} paddingVertical={PaddingSize.Wide3} paddingHorizontal={PaddingSize.Wide2} shouldAddGutters={true} defaultGutter={PaddingSize.Wide1}>
-      <Text variant='header1'>{'Million Dollar Token Page (MDTP)'}</Text>
-      <Markdown source={text} />
+      <Text variant='header1'>{'Million Dollar Token Page'}</Text>
+      <Markdown source={mainText} />
+      <Markdown source={learnMoreText} />
     </Stack>
   );
 };
