1d3a259a29f4a2aa68ee52fe4aef5ecf20da6a7a
Krishan Patel <krishan711@gmail.com>
2021-09-06 18:17:48 +0100

    .

--- app/src/util/chainUtil.ts
+++ app/src/util/chainUtil.ts
@@ -29,7 +29,7 @@ export enum ChainId {
 export const DEFAULT_CHAIN_ID = ChainId.Rinkeby;
 
 const validChainIdNetworkMap: Record<number, string | undefined> = {
-  // [ChainId.Rinkeby]: window.KRT_NEW_CONTRACT ? 'rinkeby6' : 'rinkeby5',
+  [ChainId.Rinkeby]: window.KRT_NEW_CONTRACT ? 'rinkeby6' : undefined,
   [ChainId.Mainnet]: 'mainnet1',
 };
 
