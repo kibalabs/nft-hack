d027a0fccef2ea4648045b5f7693c0fa48787f28
Krishan Patel <krishan711@gmail.com>
2021-11-21 21:07:41 +0000

    .

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,14 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
-import { App } from './App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
+import { App } from './app';
 
 if (typeof document !== 'undefined') {
   const target = document.getElementById('root');
