71d6ef039a563818fd6a5ba1015def97379ededb
Krishan Patel <krishan711@gmail.com>
2022-04-30 16:04:17 +0100

    .

--- app/src/components/MdtpImage.tsx
+++ app/src/components/MdtpImage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { IImageProps, Image } from '@kibalabs/ui-react';
 
 export const MdtpImage = (props: IImageProps): React.ReactElement => {
-  const source = props.source.startsWith('ipfs://') ? props.source.replace('ipfs://', 'https://ipfs.infura.io/ipfs/') : props.source;
+  const source = props.source.startsWith('ipfs://') ? props.source.replace('ipfs://', 'https://kibalabs.mypinata.cloud/ipfs/') : props.source;
   return (
     <Image {...props} source={source} />
   );
